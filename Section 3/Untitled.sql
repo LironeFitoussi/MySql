371c30deae053ec771a76ff52de6f15f3a268b1e
Lirone Fitoussi <95374011+LironeFitoussi@users.noreply.github.com>
2025-04-10 19:47:13 +0300

    10/04/2025

---
+++ Section 3/Untitled.sql
@@ -0,0 +1,6 @@
+-- UPDATE cats SET breed="Shorthair"
+-- WHERE breed="Tabby"
+UPDATE employees SET current_status="laid-off", last_name="who cares"
+WHERE id=1;
+
+-- SELECT * FROM employees;
---
+++ Section 5 - CRUD/EXERCISE1.sql
@@ -0,0 +1,4 @@
+SELECT id FROM cats;
+SELECT name, breed FROM cats;
+SELECT * FROM cats WHERE breed="Tabby";
+SELECT * FROM cats WHERE cats.age=cats.cat_id;
